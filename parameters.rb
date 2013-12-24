478254dbff117621dae686daa4d6d36d3bb3337f
kazuaki MATSUO <fly.49.89.over@gmail.com>
2013-12-24 10:13:48 +0900

    initial commit

---
+++ parameters.rb
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+
+# please modify the following path to fit your app
+APP_PATH_IOS = '/Users/username/Library/Developer/Xcode/DerivedData/projectname/Build/Products/Debug-iphonesimulator/application.app'.freeze
+APP_PATH_ANDROID = '/Users/username/Documents/test.apk'.freeze
+
+TARGET_SERVER = 'http://localhost:4723'.freeze
+
+SCREENSHOT_SAVE_PATH = "#{Date::today.to_s}/".freeze
+FileUtils.mkdir_p(SCREENSHOT_SAVE_PATH) unless FileTest.exist?(SCREENSHOT_SAVE_PATH)
+
+# 絶対パスの取得
+def absolute_app_path
+    file = File.join(File.dirname(__FILE__), APP_PATH)
+    raise "App doesn't exist #{file}" unless File.exist? file
+    file
+end
+
+# capabilities for Appium
+IOS_CAPABILITIES = {
+  'browserName' => '',
+  'device' => 'iPhone',
+  'deviceName' => 'iPhone Retina (4-inch)', #only for ios simulator: 'iPhone Retina (3.5-inch)'
+  'version' => '6.1',
+  'app' => APP_PATH_IOS
+}.freeze
+
+ANDROID_CAPABILITIES = {
+  'browserName' => '',
+  'device' => 'Android',
+  'version' => '4.2',
+  'app' => APP_PATH_ANDROID,
+  'app-package' => 'com.example',
+  'app-activity' => '.views.InitialActovoty',
+  'newCommandTimeout' => 60,
+  'app-wait-activity' => '.views.InitialActovoty'
+  #'device-ready-timeout' => 5
+}.freeze
+
+SERVER_RUL = "#{TARGET_SERVER}/wd/hub".freeze
---
+++ senario/sample_ios.rb
@@ -0,0 +1,61 @@
+# -*- encoding: utf-8 -*-
+
+require 'rubygems'
+require 'selenium-webdriver'
+require 'date'
+require 'rspec'
+
+require '../parameters' # obtain each parameters
+
+describe 'lalacall basic functional check' do
+
+  before :all do
+    # Net::Http, which is standard http module in Ruby, has a default
+    # internal timeout of 60 secound.
+    # We need to extend it because selenium use more than 60 secound
+    # in many time.
+    @client = Selenium::WebDriver::Remote::Http::Default.new
+    @client.timeout = 120 # secound
+    @driver = Selenium::WebDriver.for(:remote,
+                                      :http_client => @client,
+                                      :desired_capabilities => IOS_CAPABILITIES,
+                                      :url => SERVER_RUL)
+    @driver.manage.timeouts.implicit_wait = 10 # seconds
+    @driver_wait = Selenium::WebDriver::Wait.new :timeout => 30
+  end
+
+  after :all do
+    @driver.quit if @driver
+  end
+
+  before :each do
+  end
+
+  after :each do
+  end
+
+  # sinario
+  # 初期化
+  describe '' do
+    
+    before :all do
+
+    end
+
+    after :all do
+
+    end
+
+    before :each do
+
+    end
+
+    after :each do
+
+    end
+
+    context '' do
+      it '' do
+    end
+  end
+end
