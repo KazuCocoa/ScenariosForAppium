7fcfc28a2b8ba9495bbfffb4fb1204bb059ea6f2
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2014-11-16 10:59:18 +0900

    add error message if failed to wait

--- lib/custom_appium/commons/gesture.rb
+++ lib/custom_appium/commons/gesture.rb
@@ -3,27 +3,31 @@
 module AppiumGesture
   # find element with accessibility_id strategy
   def tap_name(element_name)
-    wait { find_element(accessibility_id: element_name) }.click
-  end
+    wait(message: "failed to tap #{element_name} with timeout") {
+      find_element(accessibility_id: element_name) }.click  end
 
   # find element with xpath strategy
   def tap_xpath(element_xpath)
-    wait { find_element(xpath: element_xpath) }.click
+    wait(message: "failed to tap #{element_xpath} with timeout") {
+      find_element(xpath: element_xpath) }.click
   end
 
   # find element with id strategy
   def tap_id(element_id)
-    wait { find_element(id: element_id) }.click
+    wait(message: "failed to tap #{element_id} with timeout") {
+      find_element(id: element_id) }.click
   end
 
   # find element with xpath strategy
   # in addition, the method search element with including words
   def tap_ele_include(element_name)
-    wait { find(element_name) }.click
+    wait(message: "failed to tap #{element_name} with timeout") {
+      find(element_name) }.click
   end
 
   def tap_text(text_name)
-    wait { text(text_name) }.click
+    wait(message: "failed to tap #{text_name} with timeout") {
+      text(text_name) }.click
   end
 
   def back_page
